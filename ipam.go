702a6a25af90c3f9dfe1386db0272664f7a5dfab
ymmt <ymmt@cybozu.com>
2018-11-01 13:00:23 +0900

    goimports

--- ipam.go
+++ ipam.go
@@ -1,14 +1,15 @@
 package neco
 
 import (
-	"github.com/cybozu-go/netutil"
 	"net"
+
+	"github.com/cybozu-go/netutil"
 )
 
-const baseNode0  = "10.69.0.0"
+const baseNode0 = "10.69.0.0"
 
 // BootNode0IP returns IP address of node0 for bootserver
 func BootNode0IP(lrn int) net.IP {
 	base := netutil.IP4ToInt(net.ParseIP(baseNode0))
-	return netutil.IntToIP4(base + 192 * uint32(lrn) + 3)
+	return netutil.IntToIP4(base + 192*uint32(lrn) + 3)
 }
