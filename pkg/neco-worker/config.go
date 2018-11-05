00cd89aa53324d6894bd64a10a8dd58ff7836f05
ymmt <ymmt@cybozu.com>
2018-11-05 14:24:57 +0900

    [worker] simplify

--- pkg/neco-worker/config.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/cybozu-go/etcdutil"
-
-type config struct {
-	HTTPProxy string           `yaml:"http_proxy"`
-	Etcd      *etcdutil.Config `yaml:"etcd"`
-}
--- pkg/neco-worker/main.go
+++ pkg/neco-worker/main.go
@@ -6,10 +6,6 @@ import (
 	"github.com/cybozu-go/well"
 )
 
-var (
-	flgConfig = flag.String("config", "/etc/neco/neco-worker.yml", "Configuration file path.")
-)
-
 func main() {
 	flag.Parse()
 	well.LogConfig{}.Apply()
