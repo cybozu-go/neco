5adf70a44602110729abd758265227230cac2c3e
ymmt <ymmt@cybozu.com>
2018-11-15 06:19:14 +0000

    fix

--- setup/setup.go
+++ setup/setup.go
@@ -169,13 +169,22 @@ func Setup(ctx context.Context, lrns []int, revoke bool) error {
 		return err
 	}
 
-	st = storage.NewStorage(ec)
-	ver, err := neco.GetDebianVersion(neco.NecoPackageName)
-	if err != nil {
-		return err
-	}
-
 	if isLeader {
+		st = storage.NewStorage(ec)
+		ver, err := neco.GetDebianVersion(neco.NecoPackageName)
+		if err != nil {
+			return err
+		}
+		for _, lrn := range lrns {
+			err = st.PutStatus(ctx, lrn, neco.UpdateStatus{
+				Version: ver,
+				Cond:    neco.CondComplete,
+			})
+			if err != nil {
+				return err
+			}
+		}
+
 		err = st.UpdateNecoRelease(ctx, ver, storage.KeyVaultUnsealKey)
 		if err != nil {
 			return err
@@ -200,10 +209,6 @@ func Setup(ctx context.Context, lrns []int, revoke bool) error {
 		if err != nil {
 			return err
 		}
-	}
-	err = st.PutStatus(ctx, mylrn, neco.UpdateStatus{Version: ver, Cond: neco.CondComplete})
-	if err != nil {
-		return err
 	}
 
 	well.CommandContext(ctx, "sync").Run()
