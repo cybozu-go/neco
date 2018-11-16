0fee10da70392e26df7277e8203728beb9b1d1b0
ymmt <ymmt@cybozu.com>
2018-11-16 05:51:34 +0000

    fix

--- worker/deb.go
+++ worker/deb.go
@@ -60,12 +60,13 @@ func InstallDebianPackage(ctx context.Context, client *http.Client, pkg *neco.De
 		return err
 	}
 
-	return well.CommandContext(ctx, "dpkg", "-i", f.Name()).Run()
+	return well.CommandContext(context.Background(),
+		"systemd-run", "-q", "dpkg", "-i", f.Name()).Run()
 }
 
 func installLocalPackage(ctx context.Context, pkg *neco.DebianPackage) error {
 	deb := fmt.Sprintf("/mnt/%s_%s_amd64.deb", pkg.Name, pkg.Release)
-	return well.CommandContext(ctx, "dpkg", "-i", deb).Run()
+	return well.CommandContext(context.Background(), "systemd-run", "-q", "dpkg", "-i", deb).Run()
 }
 
 func listGithubReleases(ctx context.Context, gh *github.Client, pkg *neco.DebianPackage) ([]*github.RepositoryRelease, error) {
