e63a8e900a83c90fa9b29dd95e22a4c6a2b487ba
Shin'ya UEOKA <ueokande@i-beam.org>
2018-12-04 23:38:03 +0000

    Fix test script

--- bin/run-dctest.sh
+++ bin/run-dctest.sh
@@ -45,7 +45,7 @@ cd \$HOME/go/src/github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME
 git checkout -qf ${CIRCLE_SHA1}
 
 cd dctest
-cp \$HOME/secrets .
+cp /home/cybozu/secrets .
 cp /assets/cybozu-ubuntu-18.04-server-cloudimg-amd64.img .
 export GO111MODULE=on
 make setup
