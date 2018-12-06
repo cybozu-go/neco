19f3ab7bdc7051bb7bc75015edb4b5f282d4d98b
Shin'ya UEOKA <ueokande@i-beam.org>
2018-12-06 02:24:21 +0000

    Set kernel-params on dctest

--- dctest/worker_test.go
+++ dctest/worker_test.go
@@ -77,6 +77,7 @@ func testWorker() {
 		execSafeAt(boot0, "sabactl", "ipam", "set", "-f", "/mnt/ipam.json")
 		execSafeAt(boot0, "sabactl", "dhcp", "set", "-f", "/mnt/dhcp.json")
 		execSafeAt(boot0, "sabactl", "machines", "create", "-f", "/mnt/machines.json")
+		execSafeAt(boot0, "sabactl", "kernel-params", "set", "console=ttyS0")
 
 		secrets, err := ioutil.ReadFile("secrets")
 		Expect(err).NotTo(HaveOccurred())
