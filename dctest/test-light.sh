89df0b89bbf250bedbcac972ec3c037f14a66f88
YujiEda <edayuzi@gmail.com>
2019-01-23 09:40:07 +0000

    [dctest] Update Makefile and add test-light.sh

---
+++ dctest/test-light.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+TARGET="$1"
+
+PLACEMAT_PID=$(echo $(pgrep placemat) | tr " " ",")
+
+while true; do
+    operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
+    if sudo -E nsenter -t ${operation_pid} -n /bin/true 2>/dev/null; then break; fi
+    if ! ps -p $PLACEMAT_PID > /dev/null; then
+        echo "FAIL: placemat is no longer working."
+        exit 1;
+    fi
+    echo "preparing placemat..."
+    sleep 1
+done
+
+# obtain operation pod's pid again, because rkt's pid may change.
+sleep 3
+operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
+
+sudo -E nsenter -t ${operation_pid} -n sh -c "export PATH=$PATH; $GINKGO $SUITE" -focus="${TARGET}"
+RET=$?
+
+exit $RET
