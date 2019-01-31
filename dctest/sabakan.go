fd78378a93c187a1a020594c9b0cfceb0750a2a0
mitz <mitz@cybozu.co.jp>
2019-01-31 15:33:35 +0900

    Use generated sabakan_ipam.json

--- dctest/sabakan.go
+++ dctest/sabakan.go
@@ -9,8 +9,8 @@ import (
 func TestSabakan() {
 	It("should initialize sabakan", func() {
 		By("setting configrations")
-		execSafeAt(boot0, "sabactl", "ipam", "set", "-f", "/mnt/ipam.json")
-		execSafeAt(boot0, "sabactl", "dhcp", "set", "-f", "/mnt/dhcp.json")
+		execSafeAt(boot0, "sabactl", "ipam", "set", "-f", "/etc/neco/sabakan_ipam.json")
+		execSafeAt(boot0, "sabactl", "dhcp", "set", "-f", "/usr/share/neco/dhcp.json")
 		execSafeAt(boot0, "sabactl", "kernel-params", "set", "console=ttyS0")
 
 		// machines will be registered later in cke_test.go.
