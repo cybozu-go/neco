c79458324c3b1250c99d857d46709cc891d619e6
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2019-02-05 19:57:21 +0900

    [worker] Run the compaction test separately from others

---
+++ worker/compaction_test.go
@@ -0,0 +1,61 @@
+// +build !no_compaction
+
+package worker
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/cybozu-go/neco"
+	"github.com/cybozu-go/neco/storage"
+	"github.com/cybozu-go/neco/storage/test"
+	"github.com/cybozu-go/well"
+)
+
+func TestEtcdCompaction(t *testing.T) {
+	ec := test.NewEtcdClient(t)
+	defer ec.Close()
+	req := neco.UpdateRequest{Version: "1", Servers: []int{1}, Stop: true, StartedAt: time.Now()}
+	reqStr, err := json.Marshal(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ec.Put(context.Background(), storage.KeyCurrent, string(reqStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	presp := &clientv3.PutResponse{}
+	for i := 0; i < 2; i++ {
+		presp, err = ec.Put(context.Background(), "TestEtcdCompaction", string(reqStr))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	_, err = ec.Compact(context.Background(), presp.Header.Revision)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	worker := NewWorker(ec, newMock(false, 0), "1.0.0", 0)
+
+	env := well.NewEnvironment(context.Background())
+	env.Go(func(ctx context.Context) error {
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+		return worker.Run(ctx)
+	})
+	env.Stop()
+	err = env.Wait()
+	if strings.Contains(err.Error(), "compacted") {
+		t.Fatal(err)
+	}
+	if err != nil {
+		t.Log(err)
+	}
+}
