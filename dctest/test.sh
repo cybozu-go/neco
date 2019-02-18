44293bf571383845875298f679a4beac685f3240
morimoto <kenji_morimoto@cybozu.co.jp>
2019-02-18 10:30:51 +0000

    Update test script to cope with pmctl bug.

--- dctest/test.sh
+++ dctest/test.sh
@@ -6,7 +6,14 @@ TAGS="$2"
 PLACEMAT_PID=$(echo $(pgrep placemat) | tr " " ",")
 
 while true; do
-    if pmctl pod show operation >/dev/null 2>&1; then break; fi
+    #if pmctl pod show operation >/dev/null 2>&1; then break; fi
+    ### temporary fix; "pmctl pod show operation" succeeds with pid == 0 if placemat is not running
+    if pmctl pod show operation >/dev/null 2>&1; then
+        if [ $(pmctl pod show operation | jq .pid) -ne 0 ]; then
+            break
+        fi
+    fi
+    ### end of temporary fix
     if ! ps -p $PLACEMAT_PID > /dev/null; then
         echo "FAIL: placemat is no longer working."
         exit 1;
