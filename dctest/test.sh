d50eba1f131bff7337fd2e92122266f5f36befb6
morimoto <kenji_morimoto@cybozu.co.jp>
2019-02-18 09:41:57 +0000

    Update test script to obtain operation pod PID safely.

--- dctest/test.sh
+++ dctest/test.sh
@@ -5,8 +5,19 @@ TAGS="$2"
 
 PLACEMAT_PID=$(echo $(pgrep placemat) | tr " " ",")
 
+get_operation_pid () {
+    while true; do
+        operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
+        if [ -n "$operation_pid" ]; then
+            return
+        fi
+        echo "obtaining operation pod's pid..."
+        sleep 1
+    done
+}
+
 while true; do
-    operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
+    get_operation_pid
     if sudo -E nsenter -t ${operation_pid} -n /bin/true 2>/dev/null; then break; fi
     if ! ps -p $PLACEMAT_PID > /dev/null; then
         echo "FAIL: placemat is no longer working."
@@ -18,6 +29,6 @@ done
 
 # obtain operation pod's pid again, because rkt's pid may change.
 sleep 3
-operation_pid=$(pgrep -P $PLACEMAT_PID -f operation)
+get_operation_pid
 
 sudo -E nsenter -t ${operation_pid} -n sh -c "export PATH=$PATH; $GINKGO -focus=\"${TARGET}\" -tags=\"${TAGS}\" $SUITE_PACKAGE"
