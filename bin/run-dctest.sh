d799867e861a11ac594d3f35664574bcb38fc58e
ymmt <ymmt2005@gmail.com>
2019-02-22 02:11:54 +0900

    [mtest] add --quiet flag when pre-deleting GCE instance

--- bin/run-dctest.sh
+++ bin/run-dctest.sh
@@ -14,7 +14,7 @@ delete_instance() {
 }
 
 # Create GCE instance
-$GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} || true
+$GCLOUD compute instances delete ${INSTANCE_NAME} --zone ${ZONE} --quiet || true
 $GCLOUD compute instances create ${INSTANCE_NAME} \
   --zone ${ZONE} \
   --machine-type ${MACHINE_TYPE} \
