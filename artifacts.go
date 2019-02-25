9b84d5c6d117a607ff3875f48aa472a460e61b1a
ymmt <ymmt2005@gmail.com>
2019-02-25 17:14:37 +0900

    [ignition] use new crypt device names for sabakan 2.1

--- artifacts.go
+++ artifacts.go
@@ -8,7 +8,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.3.1", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.11.1", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "20190201.1", Private: true},
-		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.0.1.1", Private: false},
+		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.1.0-rc.1.1", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.8.1.5", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.0.0.1", Private: false},
 		{Name: "hyperkube", Repository: "quay.io/cybozu/hyperkube", Tag: "1.13.2.1", Private: false},
