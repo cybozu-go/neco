44cefd14f1fdf87c30f9c2c11bbeddbbedc94f19
cybozu-neco <neco@cybozu.com>
2019-03-15 15:34:32 +0000

    [ci skip] 2019.03.15-2900

--- artifacts_release.go
+++ artifacts_release.go
@@ -5,7 +5,7 @@ package neco
 
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
-		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.8.1", Private: false},
+		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.9.1", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.12.1", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.1.0", Private: true},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.2.0.2", Private: false},
