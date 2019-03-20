aad6f94a2ad633b0b4fc4c58b1556b917cf6d05e
dulltz <isrgnoe@gmail.com>
2019-03-20 18:51:52 +0900

    update cke to 1.13.12.1

--- artifacts.go
+++ artifacts.go
@@ -5,7 +5,7 @@ package neco
 
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
-		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.11.1", Private: false},
+		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.12.1", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.12.2", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.1.1", Private: true},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.2.0.3", Private: false},
