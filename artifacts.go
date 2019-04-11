ee0de57c04298ef67641c1246debbed728a215d1
morimoto <kenji_morimoto@cybozu.co.jp>
2019-04-11 05:16:05 +0000

    Update setup-hw to 1.2.0.

--- artifacts.go
+++ artifacts.go
@@ -7,7 +7,7 @@ var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
 		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.13.17.1", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.12.2", Private: false},
-		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.1.1", Private: true},
+		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.2.0", Private: true},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.2.0.3", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.8.1.7", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.0.3.2", Private: false},
