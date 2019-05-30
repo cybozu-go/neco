87a4db5acb579d20c1cc55f6310f864edc49eb2d
Kazuhito MATSUDA <kazuto.jinnai@gmail.com>
2019-05-30 13:55:44 +0900

    Revert "Merge pull request #284 from cybozu-go/revert-sabakan-state-setter"
    This reverts commit 417c41471c00085de16f3e65b9fd6b7225b86f6a, reversing
    changes made to 4e7a4913224ae280f0d28cbb0656c706f3e8a527.

---
+++ dctest/parts_failure.go
@@ -0,0 +1,78 @@
+package dctest
+
+import (
+	"encoding/json"
+	"errors"
+	"path"
+
+	"github.com/cybozu-go/sabakan/v2"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+// TestPartsFailure test parts failure scenario
+func TestPartsFailure() {
+	It("transitition machine state to unhealthy", func() {
+		By("changing mock server response")
+		fileName := "dummy_redfish_data.json"
+		fileContent, err := generateFileContent("Warning", "OK", "OK", "PCIeSSD.Slot.2-C", "PCIeSSD.Slot.3-C")
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAtWithInput(boot0, []byte(fileContent), "dd", "of="+fileName)
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAt(boot0, "ckecli", "scp", fileName, "cybozu@10.69.0.5:")
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAt(boot0, "ckecli", "ssh", "cybozu@10.69.0.5", "sudo", "mv", fileName, path.Join("/etc/neco", fileName))
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("checking machine state")
+		Eventually(func() error {
+			stdout, _, err := execAt(boot0, "sabactl", "machines", "get", "--ipv4", "10.69.0.5")
+			if err != nil {
+				return err
+			}
+			var machines []sabakan.Machine
+			err = json.Unmarshal(stdout, &machines)
+			if err != nil {
+				return err
+			}
+			for _, m := range machines {
+				if m.Status.State.String() != "unhealthy" {
+					return errors.New(m.Spec.Serial + " is not unhealthy:" + m.Status.State.String())
+				}
+			}
+			return nil
+		}).Should(Succeed())
+	})
+
+	It("transitition machine state to healthy", func() {
+		By("changing mock server response")
+		fileName := "dummy_redfish_data.json"
+		fileContent, err := generateFileContent("OK", "OK", "OK", "PCIeSSD.Slot.2-C", "PCIeSSD.Slot.3-C")
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAtWithInput(boot0, []byte(fileContent), "dd", "of="+fileName)
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAt(boot0, "ckecli", "scp", fileName, "cybozu@10.69.0.5:")
+		Expect(err).ShouldNot(HaveOccurred())
+		_, _, err = execAt(boot0, "ckecli", "ssh", "cybozu@10.69.0.5", "sudo", "mv", fileName, path.Join("/etc/neco", fileName))
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By("checking machine state")
+		Eventually(func() error {
+			stdout, _, err := execAt(boot0, "sabactl", "machines", "get", "--ipv4", "10.69.0.5")
+			if err != nil {
+				return err
+			}
+			var machines []sabakan.Machine
+			err = json.Unmarshal(stdout, &machines)
+			if err != nil {
+				return err
+			}
+			for _, m := range machines {
+				if m.Status.State.String() != "healthy" {
+					return errors.New(m.Spec.Serial + " is not healthy:" + m.Status.State.String())
+				}
+			}
+			return nil
+		}).Should(Succeed())
+	})
+}
---
+++ pkg/sabakan-state-setter/strategy.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"github.com/cybozu-go/sabakan/v2"
+	"github.com/prometheus/prom2json"
+)
+
+const (
+	monitorHWStatusHealth   = "0"
+	monitorHWStatusWarning  = "1"
+	monitorHWStatusCritical = "2"
+	monitorHWStatusNull     = "-1"
+	systemdUnitsFailedTag   = "systemd-units-failed"
+)
+
+func decideSabakanState(ms machineStateSource) string {
+	if ms.serfStatus == nil || ms.serfStatus.Status != "alive" {
+		return sabakan.StateUnreachable.GQLEnum()
+	}
+
+	suf, ok := ms.serfStatus.Tags[systemdUnitsFailedTag]
+	if !ok {
+		return sabakan.StateUnhealthy.GQLEnum()
+	}
+
+	if len(suf) != 0 {
+		return sabakan.StateUnhealthy.GQLEnum()
+	}
+
+	return decideByMonitorHW(ms)
+}
+
+func decideByMonitorHW(ms machineStateSource) string {
+	if ms.machineType == nil {
+		return sabakan.StateUnhealthy.GQLEnum()
+	}
+
+	if len(ms.machineType.MetricsCheckList) == 0 {
+		return sabakan.StateHealthy.GQLEnum()
+	}
+
+	if ms.metrics == nil {
+		return sabakan.StateUnhealthy.GQLEnum()
+	}
+
+	for _, checkTarget := range ms.machineType.MetricsCheckList {
+		metrics, ok := ms.metrics[checkTarget.Name]
+		if !ok {
+			return sabakan.StateUnhealthy.GQLEnum()
+		}
+
+		var res string
+		if checkTarget.Labels == nil {
+			res = checkAllTarget(metrics)
+		} else {
+			res = checkSpecifiedTarget(metrics, checkTarget.Labels)
+		}
+		if res != sabakan.StateHealthy.GQLEnum() {
+			return res
+		}
+	}
+
+	return sabakan.StateHealthy.GQLEnum()
+}
+
+func checkAllTarget(metrics machineMetrics) string {
+	for _, m := range metrics {
+		if m.Value != monitorHWStatusHealth {
+			return sabakan.StateUnhealthy.GQLEnum()
+		}
+	}
+	return sabakan.StateHealthy.GQLEnum()
+}
+
+func checkSpecifiedTarget(metrics machineMetrics, labels map[string]string) string {
+	flagExists := false
+	for _, m := range metrics {
+		if !isMetricMatchedLabels(m, labels) {
+			continue
+		}
+		if m.Value != monitorHWStatusHealth {
+			return sabakan.StateUnhealthy.GQLEnum()
+		}
+		flagExists = true
+	}
+	if !flagExists {
+		return sabakan.StateUnhealthy.GQLEnum()
+	}
+	return sabakan.StateHealthy.GQLEnum()
+}
+
+func isMetricMatchedLabels(metric prom2json.Metric, labels map[string]string) bool {
+	for k, v := range labels {
+		labelVal, ok := metric.Labels[k]
+		if !ok || v != labelVal {
+			return false
+		}
+	}
+	return true
+}
