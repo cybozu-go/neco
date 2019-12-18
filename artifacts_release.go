386f7ca52fda95bce3517947e1b7a22526a8067f
cybozu-neco <neco@cybozu.com>
2019-12-18 15:40:19 +0000

    [ci skip] 2019.12.18-7512

--- artifacts_release.go
+++ artifacts_release.go
@@ -11,7 +11,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.4.8", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.8.3.2", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.2.2.2", Private: false},
-		{Name: "coil", Repository: "quay.io/cybozu/coil", Tag: "1.1.6", Private: false},
+		{Name: "coil", Repository: "quay.io/cybozu/coil", Tag: "1.1.7", Private: false},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "3.5.27.1.6", Private: false},
 		{Name: "teleport", Repository: "quay.io/cybozu/teleport", Tag: "4.1.3.1", Private: false},
 	},
