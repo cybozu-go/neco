a3a533bfc05bcb3a404429a8fcfaa90020f692d1
Masayuki Ishii <masa213f@gmail.com>
2020-01-27 01:57:18 +0000

    fix

--- dctest/ignitions.go
+++ dctest/ignitions.go
@@ -13,22 +13,22 @@ func TestIgnitions() {
 	It("should create by-path based name for partitions on encrypted devices", func() {
 		machines, err := getMachinesSpecifiedRole("ss")
 		Expect(err).NotTo(HaveOccurred())
-
+		ssNodeIP := machines[0].Spec.IPv4[0]
 		cryptPartDir := "/dev/crypt-part/by-path/"
 
 		By("checking count of symbols")
-		stdout, stderr, err := execAt(boot0, "ckecli", "ssh", "cybozu@"+machines[0].Spec.IPv4[0], "ls", cryptPartDir)
+		stdout, stderr, err := execAt(boot0, "ckecli", "ssh", "cybozu@"+ssNodeIP, "ls", cryptPartDir)
 		Expect(err).NotTo(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 		devices := strings.Fields(strings.TrimSpace(string(stdout)))
 		Expect(devices).To(HaveLen(2))
 
 		By("checking correspondence on device and partition")
-		stdout, stderr, err = execAt(boot0, "ckecli", "ssh", "--", "cybozu@"+machines[0].Spec.IPv4[0], "sudo", "dmsetup", "deps", filepath.Join(cryptPartDir, "pci-0000\\:00\\:0a.0-p1"), "-o", "devname")
+		stdout, stderr, err = execAt(boot0, "ckecli", "ssh", "--", "cybozu@"+ssNodeIP, "sudo", "dmsetup", "deps", filepath.Join(cryptPartDir, "pci-0000\\:00\\:0a.0-p1"), "-o", "devname")
 		Expect(err).NotTo(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
-		Expect(string(stdout)).Should(ContainElement("crypt-vdc"))
+		Expect(string(stdout)).Should(ContainSubstring("crypt-vdc"))
 
-		stdout, stderr, err = execAt(boot0, "ckecli", "ssh", "--", "cybozu@"+machines[0].Spec.IPv4[0], "sudo", "dmsetup", "deps", filepath.Join(cryptPartDir, "pci-0000\\:00\\:0b.0-p1"), "-o", "devname")
+		stdout, stderr, err = execAt(boot0, "ckecli", "ssh", "--", "cybozu@"+ssNodeIP, "sudo", "dmsetup", "deps", filepath.Join(cryptPartDir, "pci-0000\\:00\\:0b.0-p1"), "-o", "devname")
 		Expect(err).NotTo(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
-		Expect(string(stdout)).Should(ContainElement("crypt-vdd"))
+		Expect(string(stdout)).Should(ContainSubstring("crypt-vdd"))
 	})
 }
