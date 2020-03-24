15feda3dd299cf3872325fed3931b9ce5e565c5a
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2020-03-24 04:06:55 +0000

    fix
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- dctest/suites.go
+++ dctest/suites.go
@@ -4,8 +4,8 @@ import . "github.com/onsi/ginkgo"
 
 // BootstrapSuite is a test suite that tests initial setup of Neco
 var BootstrapSuite = func() {
-	// cs x 6 + ss x 1 = 7
-	availableNodes := 7
+	// cs x 6 + ss x 4 = 10
+	availableNodes := 10
 	Context("setup", TestSetup)
 	Context("initialize", TestInit)
 	Context("sabakan", TestSabakan)
@@ -43,8 +43,8 @@ var FunctionsSuite = func() {
 
 // UpgradeSuite is a test suite that tests upgrading process works correctry
 var UpgradeSuite = func() {
-	// cs x 6 + ss x 1 = 7
-	availableNodes := 7
+	// cs x 6 + ss x 4 = 10
+	availableNodes := 10
 	Context("sabakan-state-setter", func() {
 		TestSabakanStateSetter(availableNodes)
 	})
