0d8592854c0c08b625ee10dbc20039df80246711
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2020-10-06 14:00:13 +0900

    Use cke 1.18.0
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- artifacts.go
+++ artifacts.go
@@ -5,7 +5,7 @@ package neco
 
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
-		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.18.0-rc.1", Private: false},
+		{Name: "cke", Repository: "quay.io/cybozu/cke", Tag: "1.18.0", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.25.1", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.7.0", Private: true},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.5.2", Private: false},
