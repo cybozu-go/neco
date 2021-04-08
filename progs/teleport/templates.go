16619faf53498886619f38a95693b633f8869a1d
binoue <banji-inoue@cybozu.co.jp>
2021-04-08 17:41:46 +0900

    reflect comments
    Signed-off-by: binoue <banji-inoue@cybozu.co.jp>

--- progs/teleport/templates.go
+++ progs/teleport/templates.go
@@ -8,7 +8,7 @@ teleport:
   data_dir: /var/lib/teleport
   auth_token: {{ .AuthToken }}
   advertise_ip: {{ .AdvertiseIP }}
-  auth_servers: [teleport-auth.teleport.svc.cluster.local:3025]
+  auth_servers: ["teleport-auth.teleport.svc:3025"]
   log:
     output: stderr
     severity: INFO
