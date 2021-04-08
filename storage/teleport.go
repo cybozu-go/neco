2e39f0e83ad2797777240432745ed72da61aac77
binoue <banji-inoue@cybozu.co.jp>
2021-04-08 17:09:00 +0900

    remove unused var and functions
    Signed-off-by: binoue <banji-inoue@cybozu.co.jp>

--- storage/teleport.go
+++ storage/teleport.go
@@ -2,32 +2,7 @@ package storage
 
 import (
 	"context"
-	"encoding/json"
 )
-
-// GetTeleportAuthServers returns auth servers.
-func (s Storage) GetTeleportAuthServers(ctx context.Context) ([]string, error) {
-	var servers []string
-	serversJSON, err := s.get(ctx, KeyTeleportAuthServers)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal([]byte(serversJSON), &servers)
-	if err != nil {
-		return nil, err
-	}
-	return servers, nil
-}
-
-// PutTeleportAuthServers stores auth servers' addresses
-func (s Storage) PutTeleportAuthServers(ctx context.Context, servers []string) error {
-	serversJSON, err := json.Marshal(servers)
-	if err != nil {
-		return err
-	}
-
-	return s.put(ctx, KeyTeleportAuthServers, string(serversJSON))
-}
 
 // GetTeleportAuthToken returns auth token
 func (s Storage) GetTeleportAuthToken(ctx context.Context) (string, error) {
