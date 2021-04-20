94d538455cf7ccf42d7a5b0367fb81c16cb371a7
Hiroshi Muraoka <h.muraoka714@gmail.com>
2021-04-20 17:29:27 +0900

    git-neco: Update zenhub graphql client to get similar to github client
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- pkg/git-neco/cmd/zenhub.go
+++ pkg/git-neco/cmd/zenhub.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"strings"
 )
 
 const zenhubGraphQLV1Endpoint = "https://api.zenhub.io/v1/graphql"
@@ -23,13 +23,18 @@ func NewZenHubClient(token string) *ZenHubClient {
 	}
 }
 
-func (zh *ZenHubClient) request(ctx context.Context, method string, url string, body string) ([]byte, error) {
-	req, err := http.NewRequest(method, url, strings.NewReader(body))
+func (zh *ZenHubClient) request(ctx context.Context, query string, vars map[string]interface{}) ([]byte, error) {
+	greq := []graphQLRequest{{Query: query, Variables: vars}}
+	data, err := json.Marshal(greq)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, zenhubGraphQLV1Endpoint, bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
 	req = req.WithContext(ctx)
-	req.Method = "POST"
 	req.Header.Set("X-Authentication-Token", zh.token)
 	req.Header.Add("Content-Type", "application/json")
 
@@ -44,76 +49,38 @@ func (zh *ZenHubClient) request(ctx context.Context, method string, url string, 
 		return nil, fmt.Errorf("status code should be 200, but got %d", resp.StatusCode)
 	}
 
-	b, err := io.ReadAll(resp.Body)
+	var gresp []graphQLResponse
+	err = json.NewDecoder(resp.Body).Decode(&gresp)
 	if err != nil {
 		return nil, err
 	}
 
-	var rl []responseItem
-	err = json.Unmarshal(b, &rl)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %s", err, b)
+	if len(gresp) != 1 {
+		return nil, fmt.Errorf("response should have only 1 item, but got %d", len(gresp))
 	}
 
-	for _, r := range rl {
-		if len(r.Errors) == 0 {
-			continue
-		}
-		return nil, fmt.Errorf("got response with errors: %s", string(b))
+	r := gresp[0]
+	if len(r.Errors) > 0 {
+		return nil, errors.New(r.Errors[0].Message)
 	}
-	return b, nil
+	return []byte(r.Data), nil
 }
 
 // Connect connect a pull request with an issue.
 func (zh *ZenHubClient) Connect(ctx context.Context, issueID, pullRequestID string) error {
-	body, err := json.Marshal(newPayload(issueID, pullRequestID))
-	if err != nil {
-		return err
+	// This query is copied from the value showen on the Network tab on Chrome
+	// Developer tool when manually connecting a PR with an issue.
+	query := "mutation CreateIssuePrConnection($input: CreateIssuePrConnectionInput!) {\n  createIssuePrConnection(input: $input) {\n    issue {\n      id\n      __typename\n    }\n    __typename\n  }\n}\n"
+	vars := map[string]interface{}{
+		"input": map[string]interface{}{
+			"issueId":       issueID,
+			"pullRequestId": pullRequestID,
+		},
 	}
 
-	_, err = zh.request(ctx, http.MethodPost, zenhubGraphQLV1Endpoint, string(body))
+	_, err := zh.request(ctx, query, vars)
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-type payloadItem struct {
-	OperationName string     `json:"operationName"`
-	Query         string     `json:"query"`
-	Variables     *variables `json:"variables"`
-}
-
-type variables struct {
-	Input *zenHubInput `json:"input"`
-}
-
-type zenHubInput struct {
-	IssueID       string `json:"issueId"`
-	PullRequestID string `json:"pullRequestId"`
-}
-
-type responseItem struct {
-	Errors []errorItem `json:"errors"`
-}
-
-type errorItem struct {
-	Message string `json:"message"`
-}
-
-// This payload can be observed by connecting PR with issue on ZenHub WebUI and
-// open Network tab of Chrome Developer Tool
-func newPayload(issueID, pullRequestID string) []payloadItem {
-	return []payloadItem{
-		{
-			OperationName: "CreateIssuePrConnection",
-			Query:         "mutation CreateIssuePrConnection($input: CreateIssuePrConnectionInput!) {\n  createIssuePrConnection(input: $input) {\n    issue {\n      id\n      __typename\n    }\n    __typename\n  }\n}\n",
-			Variables: &variables{
-				&zenHubInput{
-					IssueID:       issueID,
-					PullRequestID: pullRequestID,
-				},
-			},
-		},
-	}
 }
