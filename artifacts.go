ebac4e2767768efb8f1a19b6320b68139da75ec0
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-04-22 16:56:29 +0900

    Update coil to 2.0.7
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- artifacts.go
+++ artifacts.go
@@ -5,7 +5,7 @@ package neco
 
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
-		{Name: "coil", Repository: "ghcr.io/cybozu-go/coil", Tag: "2.0.6", Private: false},
+		{Name: "coil", Repository: "ghcr.io/cybozu-go/coil", Tag: "2.0.7", Private: false},
 		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.0.7.5", Private: false},
 		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.0.0.1", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.3.25.4", Private: false},
@@ -14,7 +14,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.9.5.2", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.7.1", Private: true},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "4.14.1", Private: false},
-		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.6.3.1", Private: false},
+		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.6.4.1", Private: false},
 	},
 	Debs: []DebianPackage{
 		{Name: "etcdpasswd", Owner: "cybozu-go", Repository: "etcdpasswd", Release: "v1.1.1"},
