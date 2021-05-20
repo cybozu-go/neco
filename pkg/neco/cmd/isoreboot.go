ee74a2f2cdde3be53730f9be917f0b3ca76005f9
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2021-05-20 15:15:20 +0900

    add `neco isoreboot` subcommand
    Signed-off-by: UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>

---
+++ pkg/neco/cmd/isoreboot.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+)
+
+var isoRebootCmd = &cobra.Command{
+	Use:   "isoreboot ISO_FILE",
+	Short: "initiate reboot from a ISO image",
+	Long: `Connect iso file to virtual DVD and schedule reboot of workers.
+
+This uses CKE's function of graceful reboot for the nodes used by CKE.
+As for the other nodes, this reboots them immediately.
+If some nodes are already powered off, this command does not do anything to those nodes.`,
+
+	Args: cobra.ExactArgs(1),
+	Run:  isoRebootRun,
+}
+
+var isoRebootGetOpts sabakanMachinesGetOpts
+
+func isoRebootRun(cmd *cobra.Command, args []string) {
+	ctx := context.Background()
+
+	uploadAssetsAndRunCommandOnWorkers(ctx, &isoRebootGetOpts, args, []string{"docker", "exec", "setup-hw", "setup-isoreboot"}, true)
+}
+
+func init() {
+	rootCmd.AddCommand(isoRebootCmd)
+	addSabakanMachinesGetOpts(isoRebootCmd, &isoRebootGetOpts)
+}
