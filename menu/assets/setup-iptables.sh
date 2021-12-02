830f11ee5aa45ce80331f4149d412732109447c8
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-12-02 15:20:50 +0900

    Add comment to setup-iptables.sh
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- menu/assets/setup-iptables.sh
+++ menu/assets/setup-iptables.sh
@@ -3,5 +3,6 @@
 # eth0 -> internet
 # eth1 -> BMC
 iptables -t nat -A POSTROUTING -o eth0 -s {{.Internet}} -j MASQUERADE
+# This rule SNAT packets destined to the internet from NTP servers so that those packets can reach the internet.
 iptables -t nat -A POSTROUTING -o eth0 -s {{.NTP}} -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
