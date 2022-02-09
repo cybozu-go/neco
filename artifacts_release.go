3afb2a7e26b12a7e0aa7b0a8901cbae97e9dd86e
cybozu-neco <neco@cybozu.com>
2022-02-09 15:46:04 +0000

    [ci skip] 2022.02.09-17290

--- artifacts_release.go
+++ artifacts_release.go
@@ -12,7 +12,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.3.0.1", Private: false},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.9.2", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.9.5.5", Private: false},
-		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.10.0", Private: true},
+		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.10.1", Private: true},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "5.2.2", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.8.2.1", Private: false},
 		{Name: "cilium", Repository: "quay.io/cybozu/cilium", Tag: "1.11.0.1", Private: false},
