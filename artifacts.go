746dc76836b2602853702202b28b238afd7b4b56
zoetrope <a.ikezoe@gmail.com>
2022-04-14 14:13:35 +0900

    Update etcd to 3.5.3
    Signed-off-by: zoetrope <a.ikezoe@gmail.com>

--- artifacts.go
+++ artifacts.go
@@ -8,7 +8,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "coil", Repository: "ghcr.io/cybozu-go/coil", Tag: "2.0.14", Private: false},
 		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.0.9.1", Private: false},
 		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.2.0.1", Private: false},
-		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.1.1", Private: false},
+		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.3.1", Private: false},
 		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.3.0.1", Private: false},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.11.0", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.9.7.1", Private: false},
