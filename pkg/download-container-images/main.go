7e1301d3307364b4b50de35f97a8e41dee72ff2e
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2023-01-27 14:43:33 +0900

    download CKE-refered images too
    Signed-off-by: UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>

--- pkg/download-container-images/main.go
+++ pkg/download-container-images/main.go
@@ -12,7 +12,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
+	"github.com/cybozu-go/cke"
 	"github.com/cybozu-go/neco"
 )
 
@@ -21,17 +23,37 @@ func usage() {
 	os.Exit(2)
 }
 
+type ImageSpec struct {
+	FullName string
+	TarName  string
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		usage()
 	}
 	downloadDir := os.Args[1]
 
+	specs := []ImageSpec{}
 	for _, img := range neco.CurrentArtifacts.Images {
-		fullName := img.FullName(true)
+		specs = append(specs, ImageSpec{
+			FullName: img.FullName(true),
+			// setup-hw-secret will be saved to cybozu-setup-hw-x.x.x.img (i.e. without "-secret"). it is intended.
+			TarName: fmt.Sprintf("cybozu-%s-%s.img", img.Name, img.Tag),
+		})
+	}
+	for _, fullName := range cke.AllImages() {
+		names := strings.Split(fullName, ":")
+		pathComponents := strings.Split(names[0], "/")
+		specs = append(specs, ImageSpec{
+			FullName: fullName,
+			TarName:  fmt.Sprintf("cybozu-%s-%s.img", pathComponents[2], names[1]),
+		})
+	}
 
-		fmt.Printf("\n*** downloading %s\n", fullName)
-		cmd := exec.Command("docker", "pull", fullName)
+	for _, img := range specs {
+		fmt.Printf("\n*** downloading %s\n", img.FullName)
+		cmd := exec.Command("docker", "pull", img.FullName)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
@@ -40,10 +62,9 @@ func main() {
 			return
 		}
 
-		// setup-hw-secret will be saved to cybozu-setup-hw-x.x.x.img (i.e. without "-secret"). it is intended.
-		imgTarName := filepath.Join(downloadDir, fmt.Sprintf("cybozu-%s-%s.img", img.Name, img.Tag))
-		fmt.Printf("*** saving %s to %s\n", fullName, imgTarName)
-		cmd = exec.Command("docker", "save", "-o", imgTarName, fullName)
+		imgTarName := filepath.Join(downloadDir, img.TarName)
+		fmt.Printf("*** saving %s to %s\n", img.FullName, imgTarName)
+		cmd = exec.Command("docker", "save", "-o", imgTarName, img.FullName)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err = cmd.Run()
