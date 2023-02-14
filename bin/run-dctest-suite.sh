0e91abab32cab4712460afbd5947d2606cde2305
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2023-02-14 13:52:42 +0000

    ci: Re-run setup to upgrade Ginkgo
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- bin/run-dctest-suite.sh
+++ bin/run-dctest-suite.sh
@@ -23,6 +23,8 @@ fi
 
 # Run dctest
 cd \${GOPATH}/src/github.com/cybozu-go/neco/dctest
+## re-run setup to upgrade ginkgo
+make setup
 exec make test TAGS=${TAG_NAME} SUITE=${SUITE_NAME} MACHINES_FILE=\${MACHINES_FILE}
 EOF
 chmod +x run.sh
