ef71087c90305ab64830ff07bf9785cb063239f4
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2023-04-17 17:03:25 +0900

    Upgrade Cilium to 1.12.8
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- artifacts.go
+++ artifacts.go
@@ -15,9 +15,9 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.13.2", Private: true},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "5.7.0.2", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.13.0.1", Private: false},
-		{Name: "cilium", Repository: "quay.io/cybozu/cilium", Tag: "1.12.4.2", Private: false},
-		{Name: "cilium-operator-generic", Repository: "quay.io/cybozu/cilium-operator-generic", Tag: "1.12.4.2", Private: false},
-		{Name: "hubble-relay", Repository: "quay.io/cybozu/hubble-relay", Tag: "1.12.4.1", Private: false},
+		{Name: "cilium", Repository: "quay.io/cybozu/cilium", Tag: "1.12.8.4", Private: false},
+		{Name: "cilium-operator-generic", Repository: "quay.io/cybozu/cilium-operator-generic", Tag: "1.12.8.1", Private: false},
+		{Name: "hubble-relay", Repository: "quay.io/cybozu/hubble-relay", Tag: "1.12.8.1", Private: false},
 		{Name: "cilium-certgen", Repository: "quay.io/cybozu/cilium-certgen", Tag: "0.1.8.1", Private: false},
 	},
 	Debs: []DebianPackage{
