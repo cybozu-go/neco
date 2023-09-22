cad9ee54161c6c9d1ae619904f8b29b26d6661fd
cybozu-neco <neco@cybozu.com>
2023-09-22 15:46:04 +0000

    [ci skip] 2023.09.22-24426

--- artifacts_release.go
+++ artifacts_release.go
@@ -6,8 +6,8 @@ package neco
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
 		{Name: "coil", Repository: "ghcr.io/cybozu-go/coil", Tag: "2.4.0", Private: false},
-		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.0.12.1", Private: false},
-		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.3.0.2", Private: false},
+		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.13.1.1", Private: false},
+		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.3.0.3", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.9.1", Private: false},
 		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.9.1.1", Private: false},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.13.2", Private: false},
