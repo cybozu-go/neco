20ddb927caffe94f34fb47218c465e8769b7031e
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2023-09-22 18:37:31 +0900

    generate-artifacts
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- artifacts.go
+++ artifacts.go
@@ -6,12 +6,12 @@ package neco
 var CurrentArtifacts = ArtifactSet{
 	Images: []ContainerImage{
 		{Name: "coil", Repository: "ghcr.io/cybozu-go/coil", Tag: "2.4.0", Private: false},
-		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.0.12.1", Private: false},
-		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.3.0.2", Private: false},
+		{Name: "bird", Repository: "quay.io/cybozu/bird", Tag: "2.13.1.1", Private: false},
+		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.3.0.3", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.9.1", Private: false},
 		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.9.1.1", Private: false},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.13.2", Private: false},
-		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.10.1.2", Private: false},
+		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.10.1.3", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.14.3", Private: true},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "5.8.0.2", Private: false},
 		{Name: "vault", Repository: "quay.io/cybozu/vault", Tag: "1.14.3.1", Private: false},
