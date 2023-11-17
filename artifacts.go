943e9aef26575aaf48ce3fa965e8a42806a3cb5e
Masayuki Ishii <masa213f@gmail.com>
2023-11-17 05:06:49 +0000

    Revert os downgrade
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- artifacts.go
+++ artifacts.go
@@ -10,7 +10,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "chrony", Repository: "quay.io/cybozu/chrony", Tag: "4.3.0.3", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.10.1", Private: false},
 		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.9.2.1", Private: false},
-		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "2.13.2", Private: false},
+		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "3.0.0", Private: false},
 		{Name: "serf", Repository: "quay.io/cybozu/serf", Tag: "0.10.1.4", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.15.0", Private: true},
 		{Name: "squid", Repository: "quay.io/cybozu/squid", Tag: "5.8.0.2", Private: false},
@@ -23,5 +23,5 @@ var CurrentArtifacts = ArtifactSet{
 	Debs: []DebianPackage{
 		{Name: "etcdpasswd", Owner: "cybozu-go", Repository: "etcdpasswd", Release: "v1.4.3"},
 	},
-	OSImage: OSImage{Channel: "stable", Version: "3602.2.0"},
+	OSImage: OSImage{Channel: "stable", Version: "3602.2.1"},
 }
