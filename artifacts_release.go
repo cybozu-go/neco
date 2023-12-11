9802ebfc33727417d2b60b119d0a06b015fcd5f0
cybozu-neco <neco@cybozu.com>
2023-12-11 15:50:08 +0000

    [ci skip] 2023.12.11-26215

--- artifacts_release.go
+++ artifacts_release.go
@@ -9,7 +9,7 @@ var CurrentArtifacts = ArtifactSet{
 		{Name: "bird", Repository: "ghcr.io/cybozu/bird", Tag: "2.14.0.1", Private: false},
 		{Name: "chrony", Repository: "ghcr.io/cybozu/chrony", Tag: "4.4.0.2", Private: false},
 		{Name: "etcd", Repository: "quay.io/cybozu/etcd", Tag: "3.5.10.1", Private: false},
-		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.9.2.1", Private: false},
+		{Name: "promtail", Repository: "quay.io/cybozu/promtail", Tag: "2.9.2.2", Private: false},
 		{Name: "sabakan", Repository: "quay.io/cybozu/sabakan", Tag: "3.0.0", Private: false},
 		{Name: "serf", Repository: "ghcr.io/cybozu/serf", Tag: "0.10.1.4", Private: false},
 		{Name: "setup-hw", Repository: "quay.io/cybozu/setup-hw", Tag: "1.15.1", Private: true},
