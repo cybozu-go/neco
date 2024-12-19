7c8a0bd55869a3ed5f385d97c65bc819ed768778
Masayuki Ishii <masa213f@gmail.com>
2024-12-19 22:29:23 +0900

    parallel execution
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- pkg/neco/cmd/collect_firmware.go
+++ pkg/neco/cmd/collect_firmware.go
@@ -12,131 +12,14 @@ import (
 	"regexp"
 	"slices"
 	"strings"
+	"sync"
 
 	"github.com/cybozu-go/log"
+	"github.com/cybozu-go/sabakan/v3"
+	"github.com/cybozu-go/well"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
-
-type fwNameConversionRule struct {
-	re      *regexp.Regexp
-	newName string
-}
-
-var fwNameConversionRuleList = map[string][]fwNameConversionRule{
-	"r6525-boot-1": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell EMC iDRAC Service Module Embedded Package .*`), "Dell EMC iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile(`Power Supply.Slot.[12]`), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .*`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Broadcom NetXtreme Gigabit Ethernet"},
-		// Disk
-		{regexp.MustCompile(`PCIe SSD in Slot [0-9] in Bay 1`), "PCIe SSD"},
-	},
-	"r6525-cs-1": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Broadcom NetXtreme Gigabit Ethernet"},
-		// Disk
-		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
-	},
-	"r6525-cs-2": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Broadcom NetXtreme Gigabit Ethernet"},
-		// Disk
-		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
-	},
-	"r7525-ss-1": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Broadcom NetXtreme Gigabit Ethernet"},
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		// Disk
-		{regexp.MustCompile(`Disk [0-9] on AHCI Controller in SL 7`), "BOSS"},
-		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
-	},
-	"r7525-ss-2": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Broadcom NetXtreme Gigabit Ethernet"},
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		// Disk
-		{regexp.MustCompile(`Disk [0-9] on AHCI Controller in SL 7`), "BOSS"},
-		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
-	},
-	"r6615-cs-1": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet \(BCM5720\) - .*`), "Broadcom NetXtreme Gigabit Ethernet (BCM5720)"},
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		// Disk
-		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
-	},
-	"r7615-ss-1": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet \(BCM5720\) - .*`), "Broadcom NetXtreme Gigabit Ethernet (BCM5720)"},
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		// Disk
-		{regexp.MustCompile(`Disk [0-9] on BOSS in SL 10`), "BOSS"},
-		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
-	},
-	"r7615-ss-2": {
-		// Dell
-		{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
-		{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
-		{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
-		// Power
-		{regexp.MustCompile("Power Supply.Slot.[12]"), "Power Supply"},
-		// NIC
-		{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet \(BCM5720\) - .*`), "Broadcom NetXtreme Gigabit Ethernet (BCM5720)"},
-		{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Intel(R) Ethernet 25G 2P E810-XXV OCP"},
-		// Disk
-		{regexp.MustCompile(`Disk [0-9] on BOSS in SL 10`), "BOSS"},
-		{regexp.MustCompile("PCIe SSD in Slot [0-9][0-9]* in Bay 1"), "PCIe SSD"},
-	},
-}
-
-type firmwareInfo struct {
-	name    string
-	version string
-}
 
 var collectFirmwareGetOpts sabakanMachinesGetOpts
 
@@ -157,114 +40,248 @@ IP is one of the IP addresses owned by the machine.`,
 		if bmcPassword == "" {
 			log.ErrorExit(errors.New("BMC_PASS is not set"))
 		}
+		collector := newFirmwareCollector(bmcUser, bmcPassword)
 
-		machines, err := sabakanMachinesGet(cmd.Context(), &collectFirmwareGetOpts)
-		if err != nil {
-			log.ErrorExit(err)
-		}
-
-		httpClient := &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		}
-
-		fwVersions := map[string][]string{} // key: device, value: versions
-		for _, m := range machines {
-			machineType := m.Spec.Labels["machine-type"]
-			rules := fwNameConversionRuleList[machineType]
-
-			fwList, err := collectFirmware(cmd.Context(), httpClient, m.Spec.BMC.IPv4, bmcUser, bmcPassword)
+		well.Go(func(ctx context.Context) error {
+			machines, err := sabakanMachinesGet(ctx, &collectFirmwareGetOpts)
 			if err != nil {
 				log.ErrorExit(err)
 			}
 
-			for _, fw := range fwList {
-				name := fw.name
-				for _, r := range rules {
-					if r.re.MatchString(fw.name) {
-						name = r.newName
-						break
+			/*
+				 "machine-type-1":
+				     "BIOS":
+					     "1.1.1": 1
+						 "2.2.2": 2
+				     "Integrated NIC":
+					     "1.2.3": 3
+				 "machine-type-2":
+				     "BIOS":
+					     "1.1.1": 3
+				     "Integrated NIC":
+					     "1.2.3": 3
+			*/
+			summary := map[string]map[string]map[string]int{}
+
+			var mu sync.Mutex
+			eg, egctx := errgroup.WithContext(ctx)
+			eg.SetLimit(10)
+			for _, m := range machines {
+				m := m
+				eg.Go(func() error {
+					fwInfoList, err := collector.fetchFirmwareInfo(egctx, &m)
+					if err != nil {
+						log.Error("failed to fetch firmware info", map[string]interface{}{"serial": m.Spec.Serial, "error": err.Error()})
+						return nil // Don't stop other goroutines
 					}
-				}
-				newVersions := append(fwVersions[name], fw.version)
-				slices.Sort(newVersions)
-				fwVersions[name] = slices.Compact(newVersions)
+
+					mu.Lock()
+					defer mu.Unlock()
+
+					for _, info := range fwInfoList {
+						fmt.Printf("%s,%s,%s,%s,%s\n", info.serial, info.machineType, info.alias, info.name, info.version)
+
+						if _, ok := summary[info.machineType]; !ok {
+							summary[info.machineType] = map[string]map[string]int{}
+						}
+						if _, ok := summary[info.machineType][info.alias]; !ok {
+							summary[info.machineType][info.alias] = map[string]int{}
+						}
+						summary[info.machineType][info.alias][info.version]++
+					}
+					return nil
+				})
 			}
-		}
+			if err := eg.Wait(); err != nil {
+				log.ErrorExit(err)
+			}
 
-		fwNames := []string{}
-		for k := range fwVersions {
-			fwNames = append(fwNames, k)
-		}
-		slices.Sort(fwNames)
-
-		for _, name := range fwNames {
-			fmt.Printf("%s: %s\n", name, strings.Join(fwVersions[name], ", "))
+			fmt.Println("# Summary")
+			machineTypeList := []string{}
+			for machineType := range summary {
+				machineTypeList = append(machineTypeList, machineType)
+			}
+			slices.Sort(machineTypeList)
+			for _, machineType := range machineTypeList {
+				fmt.Println("## " + machineType)
+				aliasList := []string{}
+				for alias := range summary[machineType] {
+					aliasList = append(aliasList, alias)
+				}
+				slices.Sort(aliasList)
+				for _, alias := range aliasList {
+					s := []string{}
+					for version, count := range summary[machineType][alias] {
+						s = append(s, fmt.Sprintf("%s(%d)", version, count))
+					}
+					slices.Sort(s)
+					fmt.Println(alias + ": " + strings.Join(s, ", "))
+				}
+				fmt.Println("")
+			}
+			return nil
+		})
+		well.Stop()
+		if err := well.Wait(); err != nil {
+			log.ErrorExit(err)
 		}
 	},
-}
-
-func collectFirmware(ctx context.Context, httpClient *http.Client, bmcAddr, bmcUser, bmcPassword string) ([]firmwareInfo, error) {
-	request := func(path string) ([]byte, error) {
-		request, err := http.NewRequest("GET", "https://"+bmcAddr+path, nil)
-		if err != nil {
-			return nil, err
-		}
-		request.SetBasicAuth(bmcUser, bmcPassword)
-
-		response, err := httpClient.Do(request)
-		if err != nil {
-			return nil, err
-		}
-		defer response.Body.Close()
-
-		return io.ReadAll(response.Body)
-	}
-
-	data, err := request("/redfish/v1/UpdateService/FirmwareInventory")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get firmware inventory collection: %w", err)
-	}
-
-	fwCollection := struct {
-		Members []struct {
-			Id string `json:"@odata.id"`
-		} `json:"Members"`
-	}{}
-	err = json.Unmarshal([]byte(data), &fwCollection)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal firmware inventory collection: %w", err)
-	}
-
-	installedFirmwarePathes := []string{}
-	for _, ent := range fwCollection.Members {
-		if strings.Contains(ent.Id, "/Installed-") {
-			installedFirmwarePathes = append(installedFirmwarePathes, ent.Id)
-		}
-	}
-
-	ret := []firmwareInfo{}
-	for _, fwPath := range installedFirmwarePathes {
-		data, err := request(fwPath)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get firmware inventory: %w", err)
-		}
-
-		fwInfo := struct {
-			Name    string `json:"Name"`
-			Version string `json:"Version"`
-		}{}
-		err = json.Unmarshal([]byte(data), &fwInfo)
-		if err != nil {
-			return nil, fmt.Errorf("failed to unmarshal firmware inventor: %w", err)
-		}
-		ret = append(ret, firmwareInfo{name: fwInfo.Name, version: fwInfo.Version})
-	}
-	return ret, nil
 }
 
 func init() {
 	collectCmd.AddCommand(collectFirmwareCmd)
 	addSabakanMachinesGetOpts(collectFirmwareCmd, &collectFirmwareGetOpts)
+}
+
+type fwNameAlias struct {
+	re    *regexp.Regexp
+	alias string
+}
+
+var fwNameCommonAliasList = []fwNameAlias{
+	// Dell
+	{regexp.MustCompile(`Dell 64 Bit uEFI Diagnostics, .*`), "Dell 64 Bit uEFI Diagnostics"},
+	{regexp.MustCompile(`Dell OS Driver Pack, .*`), "Dell OS Driver Pack"},
+	{regexp.MustCompile(`Dell EMC iDRAC Service Module Embedded Package .*`), "Dell EMC iDRAC Service Module Embedded Package"}, // old
+	{regexp.MustCompile(`Dell iDRAC Service Module Embedded Package .*`), "Dell iDRAC Service Module Embedded Package"},
+	// Power Supply
+	{regexp.MustCompile(`Power Supply.Slot.[12]`), "Power Supply"},
+	// NIC
+	{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet - .*`), "Embedded NIC"},
+	{regexp.MustCompile(`Broadcom NetXtreme Gigabit Ethernet \(BCM5720\) - .*`), "Embedded NIC"},
+	{regexp.MustCompile(`Intel\(R\) Ethernet 25G 2P E810-XXV OCP - .+`), "Integrated NIC"},
+}
+
+var fwNameAliasList = map[string][]fwNameAlias{
+	"r6525-boot-1": {
+		{regexp.MustCompile(`PCIe SSD in Slot [0-9] in Bay 1`), "PCIe SSD"},
+	},
+	"r6525-cs-1": {
+		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
+	},
+	"r6525-cs-2": {
+		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
+	},
+	"r7525-ss-1": {
+		{regexp.MustCompile(`Disk [0-9] on AHCI Controller in SL 7`), "BOSS"},
+		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
+	},
+	"r7525-ss-2": {
+		{regexp.MustCompile(`Disk [0-9] on AHCI Controller in SL 7`), "BOSS"},
+		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
+	},
+	"r6615-cs-1": {
+		{regexp.MustCompile("PCIe SSD in Slot [0-9] in Bay 1"), "PCIe SSD"},
+	},
+	"r7615-ss-1": {
+		{regexp.MustCompile(`Disk [0-9] on BOSS in SL 10`), "BOSS"},
+		{regexp.MustCompile(`Disk [0-9][0-9]* in Backplane 1 of Storage Controller in Slot 3`), "HDD"},
+	},
+	"r7615-ss-2": {
+		{regexp.MustCompile(`Disk [0-9] on BOSS in SL 10`), "BOSS"},
+		{regexp.MustCompile("PCIe SSD in Slot [0-9][0-9]* in Bay 1"), "PCIe SSD"},
+	},
+}
+
+type firmwareCollector struct {
+	client      *http.Client
+	bmcUser     string
+	bmcPassword string
+}
+
+type firmwareInfo struct {
+	serial      string
+	machineType string
+	alias       string
+	name        string
+	version     string
+}
+
+func newFirmwareCollector(bmcUser, bmcPassword string) *firmwareCollector {
+	return &firmwareCollector{
+		client: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		},
+		bmcUser:     bmcUser,
+		bmcPassword: bmcPassword,
+	}
+}
+
+func (c *firmwareCollector) request(ctx context.Context, bmcAddr, path string) ([]byte, error) {
+	request, err := http.NewRequestWithContext(ctx, "GET", "https://"+bmcAddr+path, nil) // TODO: set timeout
+	if err != nil {
+		return nil, err
+	}
+	request.SetBasicAuth(c.bmcUser, c.bmcPassword)
+
+	response, err := c.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	return io.ReadAll(response.Body)
+}
+
+func (c *firmwareCollector) fetchFirmwareInfo(ctx context.Context, machine *sabakan.Machine) ([]firmwareInfo, error) {
+	data, err := c.request(ctx, machine.Spec.BMC.IPv4, "/redfish/v1/UpdateService/FirmwareInventory")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get firmware inventory collection: %w", err)
+	}
+
+	fwInventoryCollectionResp := struct {
+		Members []struct {
+			Id string `json:"@odata.id"`
+		} `json:"Members"`
+	}{}
+	err = json.Unmarshal([]byte(data), &fwInventoryCollectionResp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal firmware inventory collection: %w", err)
+	}
+
+	installedFirmwarePathes := []string{}
+	for _, ent := range fwInventoryCollectionResp.Members {
+		if strings.Contains(ent.Id, "/Installed-") {
+			installedFirmwarePathes = append(installedFirmwarePathes, ent.Id)
+		}
+	}
+
+	serial := machine.Spec.Serial
+	machineType := machine.Spec.Labels["machine-type"]
+	aliasList := append(fwNameCommonAliasList, fwNameAliasList[machineType]...)
+
+	ret := []firmwareInfo{}
+	for _, fwPath := range installedFirmwarePathes {
+		data, err := c.request(ctx, machine.Spec.BMC.IPv4, fwPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get firmware inventory: %w", err)
+		}
+
+		fwInventoryResp := struct {
+			Name    string `json:"Name"`
+			Version string `json:"Version"`
+		}{}
+		err = json.Unmarshal([]byte(data), &fwInventoryResp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal firmware inventory: %w", err)
+		}
+
+		alias := fwInventoryResp.Name
+		for _, r := range aliasList {
+			if r.re.MatchString(fwInventoryResp.Name) {
+				alias = r.alias
+				break
+			}
+		}
+		ret = append(ret, firmwareInfo{
+			serial:      serial,
+			machineType: machineType,
+			alias:       alias,
+			name:        fwInventoryResp.Name,
+			version:     fwInventoryResp.Version,
+		})
+	}
+	return ret, nil
 }
