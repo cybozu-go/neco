0ccd50fb25eb837124629fdf9e9dace5e312ec9b
Maho Takara <takara@cybozu.co.jp>
2025-01-23 06:59:09 +0000

    test Vault policy

--- progs/vault/policies.go
+++ progs/vault/policies.go
@@ -8,70 +8,16 @@ path "auth/*"
   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 
-# List, create, update, and delete auth methods
-path "sys/auth/*"
-{
-  capabilities = ["create", "update", "delete", "sudo"]
-}
-
-# List auth methods
-path "sys/auth"
-{
-  capabilities = ["read"]
-}
-
-# Create and manage ACL policies via CLI
-path "sys/policy/*"
+# Test
+path "sys/*"
 {
   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-
-# Create and manage ACL policies via API
-path "sys/policies/acl/*"
-{
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-
-# To list policies - Step 3
-path "sys/policy"
-{
-  capabilities = ["read"]
-}
-
-# To perform Step 4
-path "sys/capabilities"
-{
-  capabilities = ["create", "update"]
-}
-
-# To perform Step 4
-path "sys/capabilities-self"
-{
-  capabilities = ["create", "update"]
 }
 
 # List, create, update, and delete key/value secrets
 path "secret/*"
 {
   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-
-# Manage secret engines broadly across Vault
-path "sys/mounts/*"
-{
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-
-# List existing secret engines
-path "sys/mounts"
-{
-  capabilities = ["read"]
-}
-
-# Read health checks
-path "sys/health"
-{
-  capabilities = ["read", "sudo"]
 }
 `
 }
